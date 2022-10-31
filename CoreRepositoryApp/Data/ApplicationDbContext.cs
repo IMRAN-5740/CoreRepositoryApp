8d8ff9f26c757544e5e4aa7bf23d22a66accd94d
imran5740 <imranbsmrstucse@gmail.com>
2022-10-31 20:45:50 +0600

    Add project files.

---
+++ CoreRepositoryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CoreRepositoryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreRepositoryApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext >options):base(options)   
+        {
+           
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ CoreRepositoryApp/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreRepositoryApp.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        [Required (ErrorMessage ="Please Enter your Product Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter your Product Description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Please Enter your Product Price")]
+        public decimal Price { get; set; }
+
+    }
+}
